310b61f755ca7fdfee04d55f3c5bf66b76af6985
SharpNeutrons <hunterparsons3@gmail.com>
2019-10-27 20:46:33 -0400

    Some stuff

--- util.py
+++ util.py
@@ -1,10 +1,19 @@
 def newNodes(n):
 	N = []
 	r1 = newRow(n)
+	for r in r1:
+		r2 = newRow(r)
 	return N
 
+def newBoardRecurse(b, n, boards):
+	if len(b) == n:
+		return (b, n, boards)
+	r1 = newRow(b[-1])
+	for r in r1:
+
+
 def newRow(prevRow):
+	if prevRow
 	return [i for i in range(prevRow+1)]
 
 def getParents(node):
-	
