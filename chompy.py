9c031d88905b4de7b79cb5896b1cc76d5298e6c0
Ty Marking <30440916+tyMarking@users.noreply.github.com>
2019-10-27 20:35:09 -0400

    possibly finished driver

--- chompy.py
+++ chompy.py
@@ -1,3 +1,5 @@
+import util
+from sortedcontainers import SortedSet
 """
 seed 1x1
 
@@ -15,17 +17,38 @@ del this node
 
 """
 """
-store: evens, expandable contraints
+store: evens, expandable contraints?
 sess data: cord list
 
 """
 
+MAX_N = 5
 
 def main():
-    pass
+    evens = seed()
+
+    for n in range(n, MAX_N+1):
+        cords = SortedSet(util.newNodes(n), key=lambda x: sum(x))
+
+        while len(cords) > 0:
+            node = cords.pop(0)
+            #remove all parents from cords
+            for parent in util.getParents(node):
+                try:
+                    cords.remove(parent)
+                except:
+                    pass
+
+            evens.add(node)
+
+
 
 def seed():
 
+    evens = set([(1)])
+    return evens
+
 
 if __name__ == '__main__':
+
     main()
