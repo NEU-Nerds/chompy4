b1646c6e0215770da056c160e90edfe08fa773d6
Ty Marking <30440916+tyMarking@users.noreply.github.com>
2019-10-27 20:06:26 -0400

    outline

--- chompy.py
+++ chompy.py
@@ -1,6 +1,30 @@
+"""
+seed 1x1
+
+for n
+
+add new nodes
+extend constraints
+add new constraints
+
+for all cords sorted by rho
+work constraints
+add expandable constraints to list
+add this node to evens
+del this node
+
+"""
+"""
+store: evens, expandable contraints
+sess data: cord list
+
+"""
+
 
 def main():
     pass
+
+def seed():
 
 
 if __name__ == '__main__':
