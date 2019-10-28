97da0ec52642f8ba4072ba6c92198c589e8ea6ef
Ty Marking <30440916+tyMarking@users.noreply.github.com>
2019-10-28 17:53:12 -0400

    Some optimizations, delete still taking all the time

--- chompy.py
+++ chompy.py
@@ -22,7 +22,7 @@ sess data: cord list
 
 """
 
-MAX_N = 5
+MAX_N = 10
 
 def main():
     evens = seed()
@@ -35,27 +35,65 @@ def main():
         #DO THIS WITH SET FUNCTIONALITY?
         for even in evens:
             #get all parents of all evens and remove try to remove them from the list
-            for parent in util.getExpandParents(even, n):
-                try:
-                    cords.remove(parent)
-                except:
-                    pass
+            cords -= util.getExpandParents(even, n)
+            # for parent in util.getExpandParents(even, n):
+            #     try:
+            #         cords.remove(parent)
+            #     except:
+            #         pass
 
         #Go through each node left (they will be even)
         while len(cords) > 0:
             node = cords.pop(0)
             #remove all parents from cords
-            parents = util.getParents(node, n)
-            for parent in parents:
-                try:
-                    cords.remove(parent)
-                except:
-                    pass
+            cords -= util.getParents(node, n)
+            # parents = util.getParents(node, n)
+            # for parent in parents:
+            #     try:
+            #         cords.remove(parent)
+            #     except:
+            #         pass
             evens.add(node)
 
-        print(str(n)+"X"+str(n)+" evens: " + str(evens))
+        # print(str(n)+"X"+str(n)+" evens: " + str(evens))
         print(str(n)+"X"+str(n)+" #evens: " + str(len(evens)))
 
+def main2():
+    evens = seed()
+
+    #list of new nodes sorted by rho
+    newNodes = []
+    for n in range(2,MAX_N+1):
+        newNodes = newNodes + util.newNodes(n)
+
+    cords = SortedSet(newNodes, key=lambda x: sum(x))
+
+    #Handle extended constraints from previous nodes very poorly
+    #DO THIS WITH SET FUNCTIONALITY?
+    for even in evens:
+        #get all parents of all evens and remove try to remove them from the list
+        cords -= util.getExpandParents(even, n)
+        # for parent in util.getExpandParents(even, n):
+        #     try:
+        #         cords.remove(parent)
+        #     except:
+        #         pass
+
+    #Go through each node left (they will be even)
+    while len(cords) > 0:
+        node = cords.pop(0)
+        #remove all parents from cords
+        cords -= util.getParents(node, n)
+        # parents = util.getParents(node, n)
+        # for parent in parents:
+        #     try:
+        #         cords.remove(parent)
+        #     except:
+        #         pass
+        evens.add(node)
+
+    # print(str(n)+"X"+str(n)+" evens: " + str(evens))
+    print(str(n)+"X"+str(n)+" #evens: " + str(len(evens)))
 
 
 def seed():
@@ -66,4 +104,4 @@ def seed():
 
 if __name__ == '__main__':
 
-    main()
+    main2()
