62204956d483baaa2c0cef2195555e935b53fc67
Ty Marking <30440916+tyMarking@users.noreply.github.com>
2019-11-03 15:49:50 -0500

    treeObj is working

--- util.py
+++ util.py
@@ -42,17 +42,7 @@ def newNodesRec(n, part):
 			nodes = nodes + newNodesRec(n, [i] + part)
 		return nodes
 
-#I thought this was necessary, now I forget why...
-"""
-def newNodesRecReversed(n, part):
-	if len(part) == n:
-		return [tuple(part)]
-	else:
-		nodes = []
-		for i in range(0, part[-1]+1):
-			nodes = nodes + newNodesRec(n, part + [i])
-		return nodes
-"""
+
 def initTree(depth, max, sigmaDict, path=[]):
 	if depth == 1:
 		ret = [True]*(max+1)
@@ -65,10 +55,10 @@ def initTree(depth, max, sigmaDict, path=[]):
 	return tree
 
 def fillTree(nodes, tree, n):
-	print("nodes: " + str(nodes))
-	for node in nodes:
-		node = list(node.path)
-		# addToTree(node, tree)
+
+	for nodeObj in nodes.copy():
+		node = list(nodeObj.path)
+
 		#make sure square form
 		for i in range(n-len(node)):
 			node.append(0)
@@ -101,6 +91,7 @@ def fillTree(nodes, tree, n):
 				#if i the only row with that val then baseparent is the only parent for this d
 				#(there are no subsequent rows to work through)
 				if j == i - 1:
+					# print("parent: " + str(baseParent))
 					addToTree(baseParent, tree)
 				#else go through recursively all the below rows
 				else:
@@ -111,174 +102,178 @@ def fillTree(nodes, tree, n):
 					# print(f"max: {max}")
 					#get all the possiblites for the rows under the top row with the same value
 					subParents = getParentsRec(baseParent[j+1:i+1] , max)
-					# print(f"subParents: {subParents}")
 					for sub in subParents:
 						#combining the sub possiblity with the rest of the board
-						# print(f"\nsub: {sub}")
-						# print(f"baseParent: {baseParent}")
 						newParent = baseParent[:j+1] + sub + baseParent[i+1:]
+
 						addToTree(newParent, tree)
 			#sets the next row to work on to be the row above the top same row
 			i = j
 
-def addToTree(leaf, tree):
-	branch = tree.rootNode
-	# print(f"\nleaf: {leaf}")
-	# print(f"tree: {tree}")
-	for item in leaf[:-1]:
-		branch = branch[item]
-		# print(f"branch: {branch}")
-	branch[leaf[-1]].setOdd()
 
-def getSigmaEvens(tree, sigma, n, path=[]):
-	if len(path) == n-1:
-		if not (sigma >= len(tree)) and tree[sigma]:
-			return [path + [sigma]]
-		return []
+def addToTree(path, tree):
+	if path[-1] == 0:
+		path = cleanPath(path)
+	tree.getNode(tuple(path)).setOdd()
 
-	ret = []
-	for i in range(min(len(tree), sigma+1)):
-		ret.extend(getSigmaEvens(tree[i], sigma-i, n, path + [i]))
-	return cleanParents(ret)
+# def getSigmaEvens(tree, sigma, n, path=[]):
+# 	if len(path) == n-1:
+# 		if not (sigma >= len(tree)) and tree[sigma]:
+# 			return [path + [sigma]]
+# 		return []
+#
+# 	ret = []
+# 	for i in range(min(len(tree), sigma+1)):
+# 		ret.extend(getSigmaEvens(tree[i], sigma-i, n, path + [i]))
+# 	return cleanParents(ret)
 
-def getSigmaPaths(sigma, n):
-	paths = []
-	for x in range(ceil(sigma/n), min(sigma,n)+1):
-		paths.append([x]+getSigmaPaths(sigma-x))
-	pass
+# def getSigmaPaths(sigma, n):
+# 	paths = []
+# 	for x in range(ceil(sigma/n), min(sigma,n)+1):
+# 		paths.append([x]+getSigmaPaths(sigma-x))
+# 	pass
 
 
-def fillTreeNext(nodes, tree, n):
-	paths = getPaths(nodes, n)
-	print(f"paths: {paths}")
-	for path in paths:
-		print(f"path: {path}")
-		cBranch = tree
-		for x in path[0]:
-			cBranch = cBranch[x]
-		print(f"cBranch: {cBranch}")
-		# cB2 = cBranch
-		branches = [cBranch]
-		for xRange in path[1]:
-			# for branch in branches:
-			# 	for x in range(xRange[0], min(len(branches),xRange[1]+1)):
+# def fillTreeNext(nodes, tree, n):
+# 	paths = getPaths(nodes, n)
+# 	# print(f"paths: {paths}")
+# 	for path in paths:
+# 		# print(f"path: {path}")
+# 		cBranch = tree
+# 		for x in path[0]:
+# 			cBranch = cBranch[x]
+# 		# print(f"cBranch: {cBranch}")
+# 		# cB2 = cBranch
+# 		branches = [cBranch]
+# 		for xRange in path[1]:
+# 			# for branch in branches:
+# 			# 	for x in range(xRange[0], min(len(branches),xRange[1]+1)):
+#
+#
+# 			branches = [branch[x] for branch in branches for x in range(xRange[0], min(len(branches),xRange[1]+1)) ]
+# 		print(f"branches: {branches}")
+# 		for branch in branches:
+# 			for x in path[2]:
+# 				branch = branch[x]
+# 			print(f"addToTree {branch}, {tree}")
+# 			addToTree(branch, tree)
 
+# def getPaths(nodes, n):
+# 	paths = []
+# 	for node in nodes:
+# 		# print("node: " + str(node))
+# 		node = list(node)
+# 		#make sure square form
+# 		for i in range(n-len(node)):
+# 			node.append(0)
+#
+#
+# 		#i is the current row we are lookign at, from bottom to top
+# 		i = len(node) - 1
+# 		while i >= 0:
+# 			# print("i: " + str(i))
+# 			#j is index of first row with a different value
+# 			j = i-1
+# 			while j >= 0 and node[j] == node[i]:
+# 				j -= 1
+# 			# print("j: " + str(j))
+#
+# 			#maxDiff is difference between next non same row and the i row
+# 			max = n
+# 			#if j < 0 then same val as the top row so use n for maxDiff
+# 			if j >= 0:
+# 				max = node[j]# - node[i]
+# 			# print("max: " + str(max))
+#
+# 			min = 0
+# 			if i+1 < len(node):
+# 				min = node[i+1]
+#
+# 			path1 = []
+# 			for x in range(j+1):
+# 				path1.append(node[x])
+#
+#
+#
+#
+# 			path2 = []
+# 			#for all eqal rows
+# 			for x in range(j+1,i+1):
+# 				path2.append((min,max))
+#
+#
+# 			path3 = []
+# 			for x in range(i+1, len(node)):
+# 				path3.append(node[x])
+#
+# 			paths.append((path1,path2,path3))
+# 			i = j
+# 	return paths
+#
+#
+# def getParentsBatch(nodes, n):
+# 	parents = set([])
+# 	for node in nodes:
+# 		node = list(node)
+# 		#make sure square form
+# 		for i in range(n-len(node)):
+# 			node.append(0)
+#
+#
+# 		#i is the current row we are lookign at, from bottom to top
+# 		i = len(node) - 1
+# 		while i >= 0:
+# 			#j is index of first row with a different value
+# 			j = i-1
+# 			while j >= 0 and node[j] == node[i]:
+# 				j -= 1
+#
+# 			#maxDiff is difference between next non same row and the i row
+# 			maxDiff = 0
+# 			#if j < 0 then same val as the top row so use n for maxDiff
+# 			if j < 0:
+# 				maxDiff = n - node[i]
+# 			else:
+# 				maxDiff = node[j] - node[i]
+#
+# 			#go through each diff
+# 			for d in range(1, maxDiff+1):
+# 				#make a coppy of the node
+# 				baseParent = node[:]
+# 				#set the top of the same rows equal to its val + diff
+# 				baseParent[j+1] += d
+#
+# 				#if i the only row with that val then baseparent is the only parent for this d
+# 				#(there are no subsequent rows to work through)
+# 				if j == i - 1:
+# 					parents.add(tuple(baseParent[:]))
+# 				#else go through recursively all the below rows
+# 				else:
+# 					#the max the row can go to, set to n then adjusted if there is an above row
+# 					max = n
+# 					if j >= 0:
+# 						max = baseParent[j+1]
+# 					#get all the possiblites for the rows under the top row with the same value
+# 					subParents = getParentsRec(baseParent[j+2:i+1] , max)
+# 					for sub in subParents:
+# 						#combining the sub possiblity with the rest of the board
+# 						newParent = baseParent[:j+2] + sub + baseParent[i+1:]
+# 						parents.add(tuple(newParent))
+# 			#sets the next row to work on to be the row above the top same row
+# 			i = j
+#
+#
+#
+# 	return cleanParents(parents)
 
-			branches = [branch[x] for branch in branches for x in range(xRange[0], min(len(branches),xRange[1]+1)) ]
-		print(f"branches: {branches}")
-		for branch in branches:
-			for x in path[2]:
-				branch = branch[x]
-			print(f"addToTree {branch}, {tree}")
-			addToTree(branch, tree)
-
-def getPaths(nodes, n):
-	paths = []
-	for node in nodes:
-		# print("node: " + str(node))
-		node = list(node)
-		#make sure square form
-		for i in range(n-len(node)):
-			node.append(0)
-
-
-		#i is the current row we are lookign at, from bottom to top
-		i = len(node) - 1
-		while i >= 0:
-			# print("i: " + str(i))
-			#j is index of first row with a different value
-			j = i-1
-			while j >= 0 and node[j] == node[i]:
-				j -= 1
-			# print("j: " + str(j))
-
-			#maxDiff is difference between next non same row and the i row
-			max = n
-			#if j < 0 then same val as the top row so use n for maxDiff
-			if j >= 0:
-				max = node[j]# - node[i]
-			# print("max: " + str(max))
-
-			min = 0
-			if i+1 < len(node):
-				min = node[i+1]
-
-			path1 = []
-			for x in range(j+1):
-				path1.append(node[x])
-
-
-
-
-			path2 = []
-			#for all eqal rows
-			for x in range(j+1,i+1):
-				path2.append((min,max))
-
-
-			path3 = []
-			for x in range(i+1, len(node)):
-				path3.append(node[x])
-
-			paths.append((path1,path2,path3))
-			i = j
-	return paths
-
-
-def getParentsBatch(nodes, n):
-	parents = set([])
-	for node in nodes:
-		node = list(node)
-		#make sure square form
-		for i in range(n-len(node)):
-			node.append(0)
-
-
-		#i is the current row we are lookign at, from bottom to top
-		i = len(node) - 1
-		while i >= 0:
-			#j is index of first row with a different value
-			j = i-1
-			while j >= 0 and node[j] == node[i]:
-				j -= 1
-
-			#maxDiff is difference between next non same row and the i row
-			maxDiff = 0
-			#if j < 0 then same val as the top row so use n for maxDiff
-			if j < 0:
-				maxDiff = n - node[i]
-			else:
-				maxDiff = node[j] - node[i]
-
-			#go through each diff
-			for d in range(1, maxDiff+1):
-				#make a coppy of the node
-				baseParent = node[:]
-				#set the top of the same rows equal to its val + diff
-				baseParent[j+1] += d
-
-				#if i the only row with that val then baseparent is the only parent for this d
-				#(there are no subsequent rows to work through)
-				if j == i - 1:
-					parents.add(tuple(baseParent[:]))
-				#else go through recursively all the below rows
-				else:
-					#the max the row can go to, set to n then adjusted if there is an above row
-					max = n
-					if j >= 0:
-						max = baseParent[j+1]
-					#get all the possiblites for the rows under the top row with the same value
-					subParents = getParentsRec(baseParent[j+2:i+1] , max)
-					for sub in subParents:
-						#combining the sub possiblity with the rest of the board
-						newParent = baseParent[:j+2] + sub + baseParent[i+1:]
-						parents.add(tuple(newParent))
-			#sets the next row to work on to be the row above the top same row
-			i = j
-
-
-
-	return cleanParents(parents)
+def cleanPath(path):
+	path = list(path)
+	x = len(path) - 1
+	while x >= 0 and path[x] == 0:
+		# print("necessary?")
+		del path[x]
+		x -= 1
+	return path
 
 def cleanParents(parents):
 	retParents = set([])
@@ -293,68 +288,68 @@ def cleanParents(parents):
 		retParents.add(tuple(parent))
 
 	return retParents
-
-def getParents(node, n):
-	node = list(node)
-	#make sure square form
-	for i in range(n-len(node)):
-		node.append(0)
-
-	parents = []
-
-	#i is the current row we are lookign at, from bottom to top
-	i = len(node) - 1
-	while i >= 0:
-		#j is index of first row with a different value
-		j = i-1
-		while j >= 0 and node[j] == node[i]:
-			j -= 1
-
-		#maxDiff is difference between next non same row and the i row
-		maxDiff = 0
-		#if j < 0 then same val as the top row so use n for maxDiff
-		if j < 0:
-			maxDiff = n - node[i]
-		else:
-			maxDiff = node[j] - node[i]
-
-		#go through each diff
-		for d in range(1, maxDiff+1):
-			#make a coppy of the node
-			baseParent = node[:]
-			#set the top of the same rows equal to its val + diff
-			baseParent[j+1] += d
-
-			#if i the only row with that val then baseparent is the only parent for this d
-			#(there are no subsequent rows to work through)
-			if j == i - 1:
-				parents.append(baseParent[:])
-			#else go through recursively all the below rows
-			else:
-				#the max the row can go to, set to n then adjusted if there is an above row
-				max = n
-				if j >= 0:
-					max = baseParent[j+1]
-				#get all the possiblites for the rows under the top row with the same value
-				subParents = getParentsRec(baseParent[j+2:i+1] , max)
-				for sub in subParents:
-					#combining the sub possiblity with the rest of the board
-					newParent = baseParent[:j+2] + sub + baseParent[i+1:]
-					parents.append(newParent)
-		#sets the next row to work on to be the row above the top same row
-		i = j
-
-
-	retParents = []
-	#clean out any zereos
-	for parent in parents:
-		x = len(parent) - 1
-		while x >= 0 and parent[x] == 0:
-			del parent[x]
-			x -= 1
-		retParents.append(tuple(parent))
-
-	return retParents
+#
+# def getParents(node, n):
+# 	node = list(node)
+# 	#make sure square form
+# 	for i in range(n-len(node)):
+# 		node.append(0)
+#
+# 	parents = []
+#
+# 	#i is the current row we are lookign at, from bottom to top
+# 	i = len(node) - 1
+# 	while i >= 0:
+# 		#j is index of first row with a different value
+# 		j = i-1
+# 		while j >= 0 and node[j] == node[i]:
+# 			j -= 1
+#
+# 		#maxDiff is difference between next non same row and the i row
+# 		maxDiff = 0
+# 		#if j < 0 then same val as the top row so use n for maxDiff
+# 		if j < 0:
+# 			maxDiff = n - node[i]
+# 		else:
+# 			maxDiff = node[j] - node[i]
+#
+# 		#go through each diff
+# 		for d in range(1, maxDiff+1):
+# 			#make a coppy of the node
+# 			baseParent = node[:]
+# 			#set the top of the same rows equal to its val + diff
+# 			baseParent[j+1] += d
+#
+# 			#if i the only row with that val then baseparent is the only parent for this d
+# 			#(there are no subsequent rows to work through)
+# 			if j == i - 1:
+# 				parents.append(baseParent[:])
+# 			#else go through recursively all the below rows
+# 			else:
+# 				#the max the row can go to, set to n then adjusted if there is an above row
+# 				max = n
+# 				if j >= 0:
+# 					max = baseParent[j+1]
+# 				#get all the possiblites for the rows under the top row with the same value
+# 				subParents = getParentsRec(baseParent[j+2:i+1] , max)
+# 				for sub in subParents:
+# 					#combining the sub possiblity with the rest of the board
+# 					newParent = baseParent[:j+2] + sub + baseParent[i+1:]
+# 					parents.append(newParent)
+# 		#sets the next row to work on to be the row above the top same row
+# 		i = j
+#
+#
+# 	retParents = []
+# 	#clean out any zereos
+# 	for parent in parents:
+# 		x = len(parent) - 1
+# 		while x >= 0 and parent[x] == 0:
+# 			del parent[x]
+# 			x -= 1
+# 		retParents.append(tuple(parent))
+#
+# 	return retParents
 
 subParents = {}
 def getParentsRec(subNode, max):
@@ -385,11 +380,11 @@ def getParentsRec(subNode, max):
 
 	return parents
 
-#TODO, right now just returning all parents
-def getExpandParents(even, n):
-	parents = getParents(even, n)
-	return parents
-	#similar to the recursive generation of states.
+# #TODO, right now just returning all parents
+# def getExpandParents(even, n):
+# 	parents = getParents(even, n)
+# 	return parents
+# 	#similar to the recursive generation of states.
 
 def getChoices(board):
 	choices = [(i, j) for i in range(len(board)) for j in range(board[i])]
